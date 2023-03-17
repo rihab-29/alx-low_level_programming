3282e771e2a2de11b212d2401c2745f98f19df0c
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-17 13:48:51 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - A program that prints the size of various computer types 
+ * main - A program that prints the size of various computer types
  * Return: Always 0 (Success)
 */
 int main(void)
