c9972e091ec67761129fccc305afd690b1b195b2
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-17 05:55:28 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with the function puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts(""\Programming is like building a multilingual puzzle");
+return (0);
+}
+
