1cd83c1c54ecda1d959faf99ec530feecf95df0d
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-17 06:03:36 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
-
+}
