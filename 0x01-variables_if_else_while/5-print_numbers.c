4178dc21de64dbf6207e3fcf1bd452e6a5c30ba5
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-18 07:48:25 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: 'prints single digit numbers starting from 0'
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
