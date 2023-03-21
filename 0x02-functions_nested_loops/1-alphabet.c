aec5d706a11a8f82438142caa62f8511129635af
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-21 11:02:16 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for(n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
