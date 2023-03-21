a2a5cac95ec7b148ab5e4bade0e4264e8c3e3bf6
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-21 13:39:30 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a >= 2 && b >= 4)
+					{
+						break;
+					}
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(' ');
+					_putchar(58);
+					_putchar(' ');
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
