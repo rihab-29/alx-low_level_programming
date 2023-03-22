026657dd202e9e3e94b6e2c347a34f5a235b74ee
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-22 09:53:40 -0700

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+		sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
