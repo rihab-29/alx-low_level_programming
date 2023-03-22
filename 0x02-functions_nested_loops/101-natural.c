a50d6694a6de89cf4c31adf4eb8547265faacd08
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-22 09:23:04 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - prints the sum of all the multiples of 3 or 5 below 1024
- * return: always 0 (success)
+ * Return: always  (success)
  */
+
 int main(void)
 {
 	int i, j = 0;
