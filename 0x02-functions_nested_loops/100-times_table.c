797d9fe948c8acb535ad9652e9bbacd053863c7d
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-22 08:52:33 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,7 @@ void print_times_table(int n)
 				if (c <= 100)
 				{
 					_putchar((c / 100) + '0');
-					_putchar(((c / 10) % 10) + '0');
+					_putchar(((c / 10)) % 10 + '0');
 				}
 				else if (c <= 99 && c >= 10)
 				{
