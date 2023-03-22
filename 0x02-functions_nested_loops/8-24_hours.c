bfc1ca154555c80e58a0e280ed9c630a7e1c96e7
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-22 06:47:31 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ void jack_bauer(void)
 		b = 0;
 		while (b < 60)
 		{
-			_putchar((a / 10) + 48);
-			_putchar((a % 10) + 48);
-			_putchar(58);
-			_putchar((b / 10) + 48);
-			_putchar((b / 10) + 48);
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
 			_putchar('\n');
 			b++;
 		}
