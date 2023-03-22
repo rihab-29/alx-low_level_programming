03565c7482808d5739021571292431f8a9a55779
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-22 09:12:01 -0700

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdi.h>
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int i, j = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+		i++
+	}
+	printf("%d\n", j);
+	return (0);
+}
