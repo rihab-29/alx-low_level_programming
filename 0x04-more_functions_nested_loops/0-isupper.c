33736aed10a2904b7a714fd4d9817346c252c804
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-27 15:25:07 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,17 @@
 
 /**
  * _isupper - checks for uppercase character
- *
  * @c: input for alphabet
- *
- * Return: 1 if it is uppercase and, 0 if not
+ * Return: 1 if it is uppercase 0 if not
  */
-
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,2 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
-
-#endif
-
