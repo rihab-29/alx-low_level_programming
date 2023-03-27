17010f408dfd66dadea6a22334883105d4cede5c
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-27 13:17:11 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input string
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int a = 0;
+	char *b = str;
+	int c;
+
+	while (*b != '\0')
+	{
+		b++;
+		len++;
+	}
+	a = len - 1;
+	for (c = 0; c <= a; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_ptchar(str[c]);
+		}
+	}
+	_putchar('\n');
+}
