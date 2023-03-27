699700b5ff437d7387091673e657dbd58f724449
rihab-29 <rihab.jrandi02@gmail.com>
2023-03-27 15:14:23 -0700

    11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid passwords for
+ * the program 101-crackme
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int psw[100];
+	int i, n;
+	int sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		psw[i] = rand() % 78;
+		sum += (psw[i] + '0');
+		putchar(psw[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
