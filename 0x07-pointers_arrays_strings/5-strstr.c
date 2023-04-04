1fa17f2f5123d071a2406400a8802800c9ae1c72
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-04 16:28:29 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needlr[i])
+		}
+		haystack++;
+	}
+	return ('\0');
+}
