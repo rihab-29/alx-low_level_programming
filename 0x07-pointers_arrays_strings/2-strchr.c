bb5ebcf2720fd833d4293ece74c6ad45edb2c33b
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-04 15:30:40 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character in the string
+ * Return: a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
