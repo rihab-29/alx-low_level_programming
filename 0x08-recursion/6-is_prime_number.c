3cdcc94e9690494270a1f8e4e5a51c0d751bfc77
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-05 17:30:52 -0700

    0x08

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 
-int checker(int n, int d);
+int checker(int n, int i);
 
 /**
  * checker - checks if number is prime
  * @n: number
- * @d: divisor
- * Return: if devisible returns 0
+ * @i: number of iterations
+ * Return: if n is prime returns 0
  * if no returns 1
  */
-int checker(int n, int d)
+int checker(int n, int i)
 {
-	if (n % d == 0)
-		return (0);
-	if (d == n / 2)
+	if (i == 1)
 		return (1);
-	return (checker(n, d + 1));
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (checker(n, i - 1));
 }
 
 /**
  * is_prime_number - returns 1 if the input int is a prime number
  * @n: input number
- * Return: integer
+ * Return: if n is prime returns 1
+ * if not returns 0
  */
 int is_prime_number(int n)
 {
-	return (checker(n, n + 1));
+	if (n <= 1)
+		return (0);
+	return (checker(n, n - 1));
 }
