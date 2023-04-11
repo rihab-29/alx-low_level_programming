4478c1a3749652c74758ea2c69a41cc29662cd7e
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-10 18:06:42 -0700

    0x0A

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,28 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, j, r;
+	if (argc == 2)
+	{
+	int i, l = 0, money = atoi(argv[1]);
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	for (i = 0; i < 5; i++)
+	{
+		if (money >= coins[i])
+		{
+			l += money / coins[i];
+			money = money % coins[i];
+			if (money % coins[i] == 0)
+			{
+				break;
+			}
+		}
+	}
+	printf("%d\n", l);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	r = 0;
-	if (num < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		do {
-			r++;
-			num -= coins[j];
-		} while (num >= coins[j]);
-	}
-	printf("%d\n", r);
 	return (0);
 }
