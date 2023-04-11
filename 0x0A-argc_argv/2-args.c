f3719b7330f8adbfe74e50079821edfcbdae9826
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-10 17:19:20 -0700

    0x0A

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number
+ * @argv: array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
