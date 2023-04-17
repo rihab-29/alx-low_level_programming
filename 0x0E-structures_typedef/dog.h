13fa001c9d879f10f703cc79b45e8e9140e3846f
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-17 16:31:51 -0700

    0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: a structure to define a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
