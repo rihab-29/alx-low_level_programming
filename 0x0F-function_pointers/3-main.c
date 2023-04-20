a78928273dd56ccc997acf3d6519281d8491fabd
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-19 18:24:07 -0700

    0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,28 +1,38 @@
+#include "function_pointers.h"
 #include "3-calc.h"
 
 /**
  * main - checks the code
- * @argv: number pf args
- * @argc: argument vector
+ * @argc: number pf args
+ * @argv: argument vector
  * Return: always 0
  */
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*op_func)(int, int), a, b;
-
+	int num1, num2;
+	char *op;
+	
 	if (argc != 4)
-		printf("Error\n"), exit(98);
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("Error\n"), exit(99);
-
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(100);
-
-	printf("%d\n", op_func(a, b));
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
