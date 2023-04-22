143e078c577f673ab8917da426b960a21853c140
rihab-29 <rihab.jrandi02@gmail.com>
2023-04-21 20:47:59 -0700

    0x10

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	va_start(ap, n);
 	while (i--)
-		printf("%s%S", (str = va_arg(ap, char *)) ? str : "(nil)",
+		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
 				i ? (separator ? separator : "") : "\n");
 	va_end(ap);
 }
