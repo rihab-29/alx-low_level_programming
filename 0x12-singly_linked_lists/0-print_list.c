e6498cbbd67cbe1f0f322c86ca18d1d19b23d9b1
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 15:18:22 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (!s)
+	{
+		return (0);
+	}
+	do {
+		i++;
+	} while (*s++);
+
+	return (i);
+}
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: header (pointer to the first node)
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->s), h->s ? h->s : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+read me file for the singly_linked_lists
