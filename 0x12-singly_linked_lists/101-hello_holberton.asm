b3692be4646a2cd7bf0e163ff911a73b9563ece0
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 19:47:59 -0700

    holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,11 +2,11 @@ section        .text
        global  main
        extern  printf
 
-main
+main:
        mov     edi, message
        mov     eax, 0
        xor     eax, eax
        call    printf
        ret
 section        .data
-       mesage db 'Hello, Holberton', 0xa, 0
+       message db 'Hello, Holberton', 0xa, 0
