98d130bafbda6f1735fa5222566fd5dc30bb99a6
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 17:28:20 -0700

    new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,8 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	new_node = malloc(sizeof(list_t));
 	unsigned int length = 0;
 
 	do {
@@ -26,5 +27,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
