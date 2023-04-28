5ccf2778a37596c7f8dcf7a00d21581a9a46a52a
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 19:30:13 -0700

    constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void 100_first(void) __attribute__ ((constructor));
 void 100_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+		"I bore my house upon my back!\n");
 }
