ab7000591979907661d2e94ae7f6122f20306702
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 19:05:52 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,6 +23,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->len = len;
 	new_node->next = NULL;
 
