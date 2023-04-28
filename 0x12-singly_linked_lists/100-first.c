603b89c44a2225468c16ffe58ac84fbbf4a29559
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 19:23:18 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * 100_firs - prints before the main() is executed
+ *
+ */
+void 100_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
