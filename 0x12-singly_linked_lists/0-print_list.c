fe3a23fa401b74bcfa864eda1cb3f9ea564d6cb6
rihab-29 <rihab.jronedi29@gmail.com>
2023-04-27 18:30:11 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%lu] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		else if (!h->str)
 			printf("[0] (nil)\n");
 		h = h->next;
 		n++;
-	} 
+	}
 	return (n);
 }
