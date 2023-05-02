9eac56c99f2842266a24d06495f8ca829efbbaee
rihab-29 <rihab.jronedi29@gmail.com>
2023-05-02 16:14:40 -0700

    reverses a listint_t

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,11 +10,6 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *node_pr = NULL;
 	listint_t *next_node = NULL;
 
-	if (!head || !*head)
-	{
-		return (NULL);
-	}
-
 	while (*head)
 	{
 		next_node = (*head)->next_node;
@@ -22,6 +17,7 @@ listint_t *reverse_listint(listint_t **head)
 		node_pr = *head;
 		*head = next_node;
 	}
+
 	*head = node_pr;
 
 	return (*head);
