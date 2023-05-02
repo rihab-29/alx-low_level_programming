c6d9761b71c538301385453544f963cfc4067f45
rihab-29 <rihab.jronedi29@gmail.com>
2023-05-02 15:09:59 -0700

    insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	if (idx == NULL)
+
+	if (!idx)
 	{
 		new_node->next = *head;
 		*head = new_node;
