99041afaccdb6418557388d069a696f50f05f764
rihab-29 <rihab.jronedi29@gmail.com>
2023-05-04 18:36:35 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,13 +10,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i = 63;
 	int filp = 0;
-	unsigned long int b;
 	unsigned long int a = n ^ m;
 
 	while (i >= 0)
 	{
-		b = a >> 1;
-		if (b & 1)
+		a = a >> i;
+		if (a & 1)
 		{
 			flip++;
 		}
