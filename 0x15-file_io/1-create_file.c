b4f7ed9071e12af6d2e58e119ec352542ef561eb
rihab-29 <rihab.jronedi29@gmail.com>
2023-05-08 16:09:46 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_CREAT | O_RDWR |O_TRUNC, 0600);
+	int fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	int bytes_write = write(fd, text_content, length);
 	int length = 0;
 
