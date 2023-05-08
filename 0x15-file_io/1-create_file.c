58c9611281c83169a096388451e277d405ef8378
rihab-29 <rihab.jronedi29@gmail.com>
2023-05-08 16:15:45 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	int bytes_write = write(fd, text_content, length);
+	int fd, bytes_write;
 	int length = 0;
 
 	if (filename == NULL)
@@ -24,6 +23,9 @@ int create_file(const char *filename, char *text_content)
 			length++;
 		}
 	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_write = write(fd, text_content, length);
 
 	if (fd == -1 || bytes_write == -1)
 	{
