4d73f50d2c08057b14800c9a5a8841b33a8c820a
rihab-29 <rihab.jrandi02@gmail.com>
2023-07-21 05:44:17 -0700

    fixing 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int wr, len = 0;
 
 	if (!filename)
-		return(-1);
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
