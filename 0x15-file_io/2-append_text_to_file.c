25e96a1b4f35d800c9dcd70b096e25c8d1d26756
rihab-29 <rihab.jrandi02@gmail.com>
2023-07-22 04:18:54 -0700

    fixing 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,8 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	fd = open(feliname, O_WRONLY | O_APPEND);
-	wr = (fd, text_content, len);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)
 		return (-1);
